48a1e596147967e6b8b5cb105447d7642d93c77e
Nick <white105@my.canisius.edu>
2017-04-29 15:31:23 -0400

    Added functionality of making JSON requests to datamuse. Next we will implement this data with the autocomplete widget

--- widget/app.js
+++ widget/app.js
@@ -1,22 +1,33 @@
+window.addEventListener('load', init());
+
 jQuery.ui.autocomplete.prototype._resizeMenu = function () {
   let ul = this.menu.element;
   ul.outerWidth(this.element.outerWidth());
   ul.outerHeight(this.element.outerHeight() * 6);
 }
 
-var webURL = "https://api.datamuse.com";
+//functions to get json data from datamuse
+function loadJSON(callback) {   
 
-let currentString = $("#tags").html();
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'https://api.datamuse.com/words?ml=spoon&sp=*a&max=10', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
 
-$.ajax(webURL, {
-    success: function(response) {
-        
-    },
-    data: {"currentString": currentString},
-    error: function (request, errorType, errorMessage) {
-        alert("Error: " + errorType + " with message " + errorMessage);
-    }
-});
+function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+    console.log(actual_JSON);
+ });
+}
 
 $( function() {
     var availableTags = [
