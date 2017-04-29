54a96bd8d186155a3def03ec7391db4877fc5201
Nick <white105@my.canisius.edu>
2017-04-29 19:58:54 -0400

    Started converting to SASS

--- widget/app.js
+++ widget/app.js
@@ -1,53 +1,21 @@
 $(document).ready(function() {
-	// if text input field value is not empty show the "X" button
 	$("#tags").keyup(function() {
 		$("#x").fadeIn();
 		if ($.trim($("#tags").val()) == "") {
 			$("#x").fadeOut();
 		}
 	});
-	// on click of "X", delete input field value and hide "X"
 	$("#x").click(function() {
 		$("#tags").val("");
 		$(this).hide();
 	});
 });
 
-
-window.addEventListener('load', init());
-
 jQuery.ui.autocomplete.prototype._resizeMenu = function () {
   let ul = this.menu.element;
   ul.outerWidth(this.element.outerWidth());
   ul.outerHeight(this.element.outerHeight() * 6);
 }
-
-//functions to get json data from datamuse
-function loadJSON(callback) {   
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'https://api.datamuse.com/words?ml=spoon&sp=*a&max=10', true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
-
-function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    console.log(actual_JSON);
-    return actual_JSON;
- });
-}
-
-init();
-
-var jsonData = init();
-console.log(jsonData);
 
 $( function() {
     var availableTags = [
@@ -78,3 +46,12 @@ $( function() {
       source: availableTags
     });
   } );
+
+
+function noogleSearch() {
+    console.log(document.getElementById("tags").innerHTML);
+}
+
+function feelingLucky() {
+    alert("Yeah too bad you're not lucky");
+}
